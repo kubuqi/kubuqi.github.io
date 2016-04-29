e36263f777996e5580387577fd0a4ab6e3abf8be
kubuqi <kubuqi@gmail.com>
2016-04-28 22:50:43 -0300

    Add comments for Google Analytics

--- javascripts/script.js
+++ javascripts/script.js
@@ -51,10 +51,12 @@ $(document).ready(function(){
 });
 })(jQuery)
 
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+// Googole Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
   ga('create', 'UA-77123603-1', 'auto');
   ga('send', 'pageview');
